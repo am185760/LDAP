cdbdf5eba651733fa3480adfc6be3e5dce1f28f2
am185760_ncratmco <AbdulJabbar.Malik@ncratleos.com>
2026-04-03 19:08:04 +0500

    all files bckp

---
+++ LDAP/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LDAP.Services;
+
+
+Users users = new();
+Groups groups = new();
+Rights rights = new();
+
+users.GetAllUsers();
+
+//rights.HandleRights();
+//users.HandleUsers();
+//groups.HandleGroups();
+
+Console.ReadLine();
